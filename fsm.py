b2c06dfb773732739fdea1f5d8c4386c1e69d583
Leandro E. Colombo Viña <colomboleandro@bitson.com.ar>
2018-01-23 11:54:28 -0300

    Adding TurnstileMachine

--- fsm.py
+++ fsm.py
@@ -179,54 +179,8 @@ class Performance:
         return 'Performance: {}, Mode: {}'.format(self.name, self.mode)
 
 
-class TurnstileMachine:
-    def __init__(self, name): #, performance='NORMAL', mode='ENTRADA'):
+class TurnstileMachine(Machine):
+    def __init__(self, name, performance='NORMAL', mode='ENTRADA'):
+        super().__init__(initial='stand_by')
         self.name = name
-        # self.performance = Performance(name=performance, mode=mode)
-        self.access_granted = False
-        self.exit_granted = True
-        self.solenoid = False
-        self.machine = Machine(states=TurnstileMachine.states, initial='stand by')
-
-        self.machine.add_transition(source='stand by', trigger='input_on', dest='access_1', before='input_activate_solenoid')
-        self.machine.add_transition(source='access_1', trigger='input_off', dest='stand by', before='deactivate_solenoid')
-        self.machine.add_transition(source='access_1', trigger='output_on', dest='access_2')
-        self.machine.add_transition(source='access_2', trigger='output_off', dest='access_1')
-        self.machine.add_transition(source='access_2', trigger='input_off', dest='access_3')
-        self.machine.add_transition(source='access_3', trigger='output_off', dest='stand by', after='register_access')
-        self.machine.add_transition(source='access_3', trigger='input_off', dest='access_2')
-        self.machine.add_transition(source='stand by', trigger='output_on', dest='leave_1', before='output_activate_solenoid')
-        self.machine.add_transition(source='leave_1', trigger='input_on', dest='leave_2')
-        self.machine.add_transition(source='leave_1', trigger='output_off', dest='stand by', before='deactivate_solenoid')
-        self.machine.add_transition(source='leave_2', trigger='output_off', dest='leave_3')
-        self.machine.add_transition(source='leave_2', trigger='input_off', dest='leave_1')
-        self.machine.add_transition(source='leave_3', trigger='input_off', dest='stand by', after='register_exit')
-        self.machine.add_transition(source='leave_3', trigger='output_on', dest='leave_2')
-
-    def input_activate_solenoid(self):
-        if not self.access_granted:
-            self.solenoid = True
-            print('Activando solenoide!')
-    
-    def deactivate_solenoid(self):
-        self.solenoid = False
-        print('Solenoide desactivado')
-
-    def output_activate_solenoid(self):
-        if not self.exit_granted:
-            self.solenoid = True
-            print('Activando solenoide!')
-
-    def register_access(self):
-        if self.access_granted:
-            self.access_granted = False
-            print('Registro de entrada')
-        else:
-            print('Anomalía de entrada')
-
-    def register_exit(self):
-        if self.exit_granted:
-            self.exit_granted = False
-            print('Registro de salida')
-        else:
-            print('Anomalía de salida')
+        self.performance = Performance(name=performance, mode=mode)
